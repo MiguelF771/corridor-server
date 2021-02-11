9b588f4ac31ce319e85b78f0a16301388793f974
Miguel <miguel.fernandez.r456@gmail.com>
2021-02-11 03:15:08 -0400

    Nodemon config

--- src/public/scripts/jsonRecive.js
+++ src/public/scripts/jsonRecive.js
@@ -1,16 +1,16 @@
 function reciveUnityJson(json){
 fetch('', {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
     headers: {
       'Content-Type': 'application/json'
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: json//JSON.stringify(data) // body data type must match "Content-Type" header
+    redirect: 'follow', // manual,
+    referrerPolicy: 'no-referrer',
+    body: json
 }).then(resp => {
         if (resp.status === 200) {
             return console.log(JSON.stringify(resp.json()));
