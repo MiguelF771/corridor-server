c631560110f6dd88e0d4740e76835325982c14fd
Miguel <miguel.fernandez.r456@gmail.com>
2021-02-11 00:18:32 -0400

    Save unity to json

--- src/index.js
+++ src/index.js
@@ -4,19 +4,22 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
+
 let port = process.env.PORT || 5000;
+
 app.listen(port, () => {
     console.log('Server on port '+port);
 });
 
 app.use(bodyParser.json())
-
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public','Game')))
 
 app.get('/', (req, res) => {
     res
         .status(200)
-        .sendFile(path.join(__dirname, 'public', 'index.html'));
+        .sendFile(path.join(__dirname, 'public', 'Game','index.html'))
+        //.sendFile(path.join(__dirname, 'public', 'Game/index.html'));
 });
 
 app.get('/stats', (req, res) => {
@@ -27,13 +30,19 @@ app.post('/', async (req, res) => {
 
     let time = new Date();
     let dataUser = req.body;
-    let file = path.join(__dirname, 'dateUsers', dataUser.userData.Name+'-'+time.getTime().toString()+'.json');
-    file = file.replace(/\s/g,'-');
-    await fs.writeFile(file, JSON.stringify(dataUser), function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-    });
+    console.log(req.body);
+    try {
+        let file = path.join(__dirname, 'dateUsers', dataUser.userData.Name+'-'+time.getTime().toString()+'.json');
+        file = file.replace(/\s/g,'-');
+        await fs.writeFile(file, JSON.stringify(dataUser), function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });
 
+    } catch (error) {
+        console.log(error)
+    }
     res
+        .status(200)
         .send('Ok');
 });
---
+++ src/public/scripts/jsonRecive.js
@@ -0,0 +1,26 @@
+function reciveUnityJson(json){
+fetch('', {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: json//JSON.stringify(data) // body data type must match "Content-Type" header
+}).then(resp => {
+        if (resp.status === 200) {
+            return console.log(JSON.stringify(resp.json()));
+        } else {
+            window.alert("Status: " + resp.status)
+            return Promise.reject("server")
+        }
+    })
+    .catch(err => {
+        if (err === "server") 
+        return console.error(err);
+    })
+ }
